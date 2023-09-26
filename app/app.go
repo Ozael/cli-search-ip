8c900d8f1fd40c5976262b45c3f7308a2fbbbb32
Ozael <ozaelvf@gmail.com>
2023-09-26 14:47:27 -0300

    initial implementation

--- app/app.go
+++ app/app.go
@@ -2,6 +2,11 @@ package app
 
 import "github.com/urfave/cli"
 
+// Generate will return the app
 func Generate() *cli.App {
+	app := cli.NewApp()
+	app.Name = "CLI app"
+	app.Usage = "Search IPs and Names of Servers"
 
+	return app
 }
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"cli-search-ip/app"
+	"log"
+	"os"
+)
 
+func main() {
+	cli_app := app.Generate()
+	if err := cli_app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
